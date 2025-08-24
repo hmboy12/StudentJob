e5858ff8b07d62eee1fbfa8c4d8bd5d6e04f3a27
hmboy12 <frederikhvidbjergmikkelsen@gmail.com>
2025-08-24 21:59:09 +0200

    Final update

--- frederik.js
+++ frederik.js
@@ -7,13 +7,11 @@ function closePage(id) {
 }
 
 function showPage(pageId) {
-  // Luk alle overlays først
   const overlays = document.querySelectorAll('.overlay');
   overlays.forEach(overlay => {
     overlay.classList.remove('active');
   });
 
-  // Åben det valgte overlay
   const page = document.getElementById(pageId);
   page.classList.add('active');
 }
